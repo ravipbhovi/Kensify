02adb4756d8527a8e8abd8b267032f7a02addcf5
AnDev Pro <91153892+andevpro@users.noreply.github.com>
2023-06-06 01:56:24 +0530

    Update SplashActivity.java

--- SplashActivity.java
+++ SplashActivity.java
@@ -2,26 +2,19 @@ package com.andevpro.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-import android.widget.LinearLayout;
 
 public class SplashActivity extends AppCompatActivity {
 
-    Handler handler;
-    LinearLayout from_ll;
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        
-
     }
 
     public void setActivityWindow(){
        
     }
+    
 }
