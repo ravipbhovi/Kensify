2dfff4152abbc3915ff5d578b91dd282f4cd1c6f
AnDev Pro <91153892+andevpro@users.noreply.github.com>
2023-06-06 02:00:57 +0530

    Update CommonActivity.java

--- CommonActivity.java
+++ CommonActivity.java
@@ -1,17 +1,7 @@
 package com.andevpro.app;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-
-public class CommonActivity extends AppCompatActivity {
-
-   
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-       
-    }
+public class CommonActivity {
 
     public void setActivityWindow(){
        
