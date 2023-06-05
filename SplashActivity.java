00e4d627954795fb5e963675bb84e7b5270d7e73
AnDev Pro <91153892+andevpro@users.noreply.github.com>
2023-06-06 01:53:33 +0530

    Update SplashActivity.java

--- SplashActivity.java
+++ SplashActivity.java
@@ -1,4 +1,4 @@
-package com.kensify.vmt;
+package com.andevpro.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,41 +17,11 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(androidx.appcompat.R.style.Theme_AppCompat_Light_NoActionBar);
-        setContentView(R.layout.activity_splash);
-       setActivityWindow();
-        handler=new Handler();
-        from_ll = findViewById(R.id.from_ll);
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-               from_ll.setVisibility(View.VISIBLE);
-            }
-        },1000);
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },2000);
-
+       
 
     }
 
     public void setActivityWindow(){
-       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setNavigationBarColor(getResources().getColor(R.color.white2));
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setStatusBarColor(getResources().getColor(R.color.white2));
-        }
-
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-
+       
     }
 }
